65570608cd2816c87b42adf57b61b40ef149268c
ernestkoko <ernesteferetin@yahoo.com>
2020-04-29 21:48:49 +0100

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -9,47 +9,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.dark,
-            //x for hexadecimal, ff for opacity and then the colour
-            primaryColor: Color(0Xff009688),
+          brightness: Brightness.dark,
+          //x for hexadecimal, ff for opacity and then the colour
+          primaryColor: Color(0Xff009688),
           accentColor: Color(0Xff7C4DFF),
-        textTheme: TextTheme(
-          body1: TextStyle(
+          textTheme: TextTheme(
+              body1: TextStyle(
             fontSize: 24,
             fontStyle: FontStyle.italic,
-          )
-        )
-      ),
-
+          ))),
       home: Scaffold(
-      appBar: AppBar(
-        title: Text("Building Layout withFlutter"),
-      ),
-      body: Center(
-        child: Text('Hello Flutter Layout',
-       ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.lightbulb_outline),
-        onPressed: (){
-          print("You rang");
-        },
-      ),
-      persistentFooterButtons: <Widget>[
-        IconButton(
-          icon: Icon(Icons.add_comment),
-          onPressed: (){},
+        appBar: AppBar(
+          title: Text("Building Layout withFlutter"),
         ),
-        IconButton(
-          icon: Icon(Icons.add_alarm),
-          onPressed: (){},
+        body: Center(
+          child: Text(
+            'Hello Flutter Layout',
+          ),
         ),
-        IconButton(
-          icon: Icon(Icons.add_location),
-          onPressed: (){},
-        )
-      ],
-    ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.lightbulb_outline),
+          onPressed: () {
+            print("You rang");
+          },
+        ),
+
+        persistentFooterButtons: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_comment),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_alarm),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_location),
+            onPressed: () {},
+          )
+
+        ],
+
+      ),
     );
   }
 }
